fa2ac2388341320cc0835da4f13dcd1c0a0ff300
Mert Gungordu <mert.gungordu247@gmail.com>
2026-01-16 17:24:48 +0300

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,390 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+// Buffer to hold raw data: [0]=Temp, [1]=Vref
+uint16_t adc_buffer[2];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_DMA_Init(void);
+static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_DMA_Init();
+  MX_GPIO_Init();
+  MX_ADC1_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+	// Calibrate the ADC for better accuracy
+	HAL_ADCEx_Calibration_Start(&hadc1);
+
+	// Start the ADC in DMA mode
+	// It will fill 'adc_buffer' and loop back to start automatically
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*) adc_buffer, 2);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	char msg[64];
+
+	// 1. Define the Factory Calibration Addresses (From Datasheet)
+#define TS_CAL1_ADDR ((uint16_t*) ((uint32_t)0x1FFF75A8)) // Value at 30 degrees C
+#define TS_CAL2_ADDR ((uint16_t*) ((uint32_t)0x1FFF75CA)) // Value at 130 degrees C
+#ifndef VREFINT_CAL_ADDR
+    #define VREFINT_CAL_ADDR   ((uint16_t*) ((uint32_t)0x1FFF75AA)) // 3.0V
+#endif
+
+	while (1) {
+		// --- Step 1: Calculate Actual System Voltage (VDDA) ---
+		// We use the Internal Reference (Vref) to find out the real supply voltage
+		uint16_t vref_cal = *VREFINT_CAL_ADDR;
+		uint16_t vref_data = adc_buffer[1];
+
+		// Formula: VDDA = 3.0V * (VREF_CAL / VREF_DATA)
+		// Result in millivolts (e.g., 3300 mV)
+		uint32_t vdda_mv = (3000 * vref_cal) / vref_data;
+
+		// --- Step 2: Calculate Temperature ---
+		// We use the slope formula with the calibration points
+		int32_t ts_cal1 = (int32_t) *TS_CAL1_ADDR;
+		int32_t ts_cal2 = (int32_t) *TS_CAL2_ADDR;
+
+		// Normalize the raw reading to 3.0V reference
+		// (Raw * Actual_Voltage) / 3000mV
+		int32_t ts_data_raw = (int32_t) adc_buffer[0];
+		int32_t ts_data_corrected = (ts_data_raw * vdda_mv) / 3000;
+		// Standard Linear Interpolation Formula for STM32
+		int32_t temperature_c = ((110 * (ts_data_corrected - ts_cal1))
+				/ (ts_cal2 - ts_cal1)) + 30;
+
+		// --- Step 3: Print Results ---
+		sprintf(msg, "Volt: %lu mV | Temp: %ld C\r\n", vdda_mv, temperature_c);
+		HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen(msg), 100);
+
+		HAL_Delay(1000);
+
+		// --- Step 4: System Protection (The "Thermostat") ---
+		// If we touch the chip and it warms up, trigger the alarm.
+		if (temperature_c > 31) {
+			// HOT! Turn on LED (PC6 for Nucleo-G031K8)
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+			strcat(msg, " [ALARM: OVERHEATING]"); // Append warning to UART log
+		} else {
+			// Cool. Turn off LED.
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+		}
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc1.Init.LowPowerAutoWait = DISABLE;
+  hadc1.Init.LowPowerAutoPowerOff = DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.NbrOfConversion = 2;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.DMAContinuousRequests = ENABLE;
+  hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc1.Init.SamplingTimeCommon1 = ADC_SAMPLETIME_160CYCLES_5;
+  hadc1.Init.SamplingTimeCommon2 = ADC_SAMPLETIME_1CYCLE_5;
+  hadc1.Init.OversamplingMode = DISABLE;
+  hadc1.Init.TriggerFrequencyMode = ADC_TRIGGER_FREQ_HIGH;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLINGTIME_COMMON_1;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_VREFINT;
+  sConfig.Rank = ADC_REGULAR_RANK_2;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_GREEN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED_GREEN_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ README.md.txt
@@ -0,0 +1,55 @@
+# STM32 System Health Monitor (ADC + DMA)
+
+A robust, non-blocking background monitoring system for the STM32G031K8. This project continuously acquires internal temperature and voltage reference data using **Direct Memory Access (DMA)**, applies factory calibration for high precision, and executes closed-loop thermal protection logic.
+
+## 🎯 Project Overview
+In safety-critical embedded systems, blocking the CPU to read sensors is unacceptable. This project demonstrates how to offload data acquisition to the hardware (DMA), allowing the CPU to focus solely on processing and control logic.
+
+* **Zero-CPU Acquisition:** Uses `ADC_Circular` mode to fill memory buffers automatically.
+* **Precision Engineering:** Implements dynamic voltage compensation to correct raw ADC readings based on actual $V_{DDA}$ supply drift.
+* **Factory Calibration:** Utilizes ST's unique factory-trimmed memory addresses to convert raw sensor data into accurate engineering units (°C and mV).
+* **Safety Logic:** "Thermostat" feature triggers a hardware alarm (LED) when the die temperature exceeds a defined threshold (31°C).
+
+## 🛠 Hardware Configuration
+* **Microcontroller:** STM32G031K8 (Nucleo-32 Board)
+* **Sensors (Internal):**
+    * `ADC_IN12`: Internal Temperature Sensor.
+    * `ADC_IN13`: Internal Voltage Reference ($V_{REFINT}$).
+* **Actuator:** On-Board LED (Green) on **PC6**.
+
+## ⚙️ Technical Implementation
+
+### 1. DMA & Circular Buffers
+Instead of polling `HAL_ADC_GetValue()`, the system configures the DMA controller to continuously transfer conversion results into a global array `adc_buffer[2]`.
+```c
+// ADC starts once, DMA manages the data transfer forever
+HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_buffer, 2);
+
+### 2. Voltage Compensation
+Raw ADC values on the STM32 depend on the supply voltage ($V_{DDA}$). Since USB power can fluctuate (e.g., 3.28V vs 3.3V), a raw reading of "932" is ambiguous.
+The system calculates the *actual* supply voltage first to normalize the sensor data:
+$$V_{DDA} = 3000 mV \times \frac{VREFINT_{CAL}}{VREFINT_{DATA}}$$
+
+### 3. Temperature Calculation (Linear Interpolation)
+The code retrieves the factory calibration constants stored in the System Memory (OTP area) during manufacturing:
+* `TS_CAL1`: Raw ADC value at 30°C (3.0V)
+* `TS_CAL2`: Raw ADC value at 130°C (3.0V)
+
+The final temperature is computed by interpolating the voltage-compensated raw value between these two calibration points.
+
+## 🚀 How to Run
+1.  Open the project in **STM32CubeIDE**.
+2.  Build and Flash to the **Nucleo-G031K8** board.
+3.  Open a Serial Terminal (115200 baud, 8-N-1).
+4.  **Observe:**
+    * Real-time Voltage and Temperature logs.
+    * **Test:** Place your finger on the MCU to raise the temp > 31°C. The Green LED will light up.
+
+## 📂 File Structure
+* `Core/Src/main.c`: Contains the Primary Control Loop, Math logic, and Hardware Initialization.
+* `Core/Inc/stm32g0xx_hal_conf.h`: HAL Configuration.
+* `HealthMonitor.ioc`: STM32CubeMX Configuration file.
+
+
+
+
